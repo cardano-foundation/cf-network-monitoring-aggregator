56d8847fb509d1ae027d84c9d05e4d3980bfb493
Giovanni Gargiulo <giovanni.gargiulo@cardanofoundation.org>
2024-10-16 12:49:27 +0100

    chore: added micrometer, which still doesn't work, and an healthcheck controller

--- src/main/java/org/cardanofoundation/networkmonitoring/CfNetworkMonitoringAggregator.java
+++ src/main/java/org/cardanofoundation/networkmonitoring/CfNetworkMonitoringAggregator.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.cardanofoundation.networkmonitoring")
 @EntityScan({
         "org.cardanofoundation.networkmonitoring.entity"
 })
